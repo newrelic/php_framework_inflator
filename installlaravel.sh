e44489f33d2c6330d63d8796a60b8dfe31f7ef88
Robert R. Henry <rrh@newrelic.com>
2014-09-25 12:18:59 -0700

    Add MIT license. Strip rrh PII from the scripts.

---
+++ installlaravel.sh
@@ -0,0 +1,317 @@
+#! /bin/bash
+
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 New Relic
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+#
+# Use this script to install laravel 4.0 and 4.1,
+# and a bootstrap starter kit from:
+#   https://github.com/andrewelkins/Laravel-4-Bootstrap-Starter-Site
+#
+# BE CAREFUL:
+#   The script removes all source prior to checking it out from github.
+#   The script first removes and then fills a mysql database
+#     named "laravel40" or "laravel41"
+#
+# There must be localhost httpd/apache2 server running to serve up
+#    localhost:9090/frameworks/laravel41/public/index.php
+#    localhost:80/frameworks/laravel41/public/index.php
+#
+# To install on a stock LAMP system
+#   bash -x ./installlaravel.sh --force --laravel41 --port 80 --root /var/www
+#
+
+set -e  # stop on first error
+set -u  # undefined variable refs are an error
+
+LAMPBIN=/opt/nr/lamp/bin
+# export PATH=${LAMPBIN}:${PATH}
+
+MYSQL=${LAMPBIN}/mysql
+MYSQL=/usr/bin/mysql
+
+PHP=${LAMPBIN}/php
+PHP=/usr/bin/php  # may be either hhvm or zend
+PHP=/usr/bin/php5  # almost surely zend
+PHP=/etc/alternatives/php  # probably zend
+
+#
+# ROOT_DIR is the file system path to the root of the served directory
+# I (rrh) haven't had much success with stock LAMP+hhvm using anything
+# other than /var/www
+#
+if [ -e /var/www ] ; then
+  ROOT_DIR=/var/www
+elif [ -e /opt/nr/htdocs ] ; then
+  ROOT_DIR=/opt/nr/htdocs
+else
+  echo can not find a plausible www root directory
+fi
+
+#
+# Hiphop apparently has a bug regarding timeouts,
+# that impacts its ability to bootstrap composer.
+#
+# for a recipe on making the HHVM php tolerate timeouts, see:
+# http://stackoverflow.com/questions/23471864/installing-composer-using-vagrant-hhvm-and-ubuntu-14-04?noredirect=1#comment36017902_23471864
+#
+# Note that hiphop php doesn't implement php -i
+#
+if echo "<?php phpinfo();" | $PHP | grep -q -i hiphop ; then
+  PHP="${PHP} -v ResourceLimit.SocketDefaultTimeout=30 -v Http.SlowQueryThreshold=30000 "
+  echo "HipHop"
+else
+  echo "Zend"
+fi
+
+do_force=no    # overwrite an existing installation
+do_yireo=no
+
+do_laravel40=no
+do_laravel41=no
+port=9090      # for nrlamp; use port=80 for regular lamp
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --root)
+      shift
+      ROOT_DIR=$1
+      ;;
+
+    --port)
+      shift
+      port=$1
+      ;;
+
+    --force|--do_force)
+      do_force=yes
+      ;;
+
+    --laravel40|--do_laravel40)
+      do_laravel40=yes
+      ;;
+
+    --laravel41|--do_laravel41)
+      do_laravel41=yes
+      ;;
+
+  esac
+  shift
+done
+
+#
+# INSTALL_DIR is the file system view to the installation area
+# BASE_URL    is the url         view to the installation area
+#
+INSTALL_DIR=${ROOT_DIR}/frameworks
+BASE_URL=http://localhost:${port}/frameworks
+
+if [ ! -e ${INSTALL_DIR} ] ; then
+  mkdir -p ${INSTALL_DIR} || true
+fi
+
+#
+# In order for laravel+artisan installation to run
+# the stock zend php installation on ubuntu,
+# php needs a pdo_mysql
+#
+# The Debian/Ubuntu systems package the PDO drivers for databases
+# with the non-PDO versions
+# (so similarly it would be php5-pgsql for both the pgsql
+# and pdo_pgsql extensions).
+#
+# Here is some useful information:
+#   http://www.howtoforge.com/ubuntu-lamp-server-with-apache2-php5-mysql-on-14.04-lts
+#  http://askubuntu.com/questions/460837/mcrypt-extension-is-missing-in-14-04-server-for-mysql
+#
+if false ; then
+  sudo apt-get install php5
+  sudo apt-get install php5-mysql php5-pgsql
+  sudo apt-get install php5-mcrypt
+  sudo apt-get install libapache2-mod-php5
+fi
+
+#
+# Usage: do_work $workdir $db_name $version
+#
+do_work() {
+  workdir=$1
+  db_name=$2
+  version=$3
+  composer_patch_function=$4
+
+  #
+  # if the work directory already exists, presume that we've been successful before
+  #
+  if [ "${do_force}" == "no" -a -e "${workdir}" ] ; then
+    echo "Re-using pre-existing laravel installation ${workdir} ${version}"
+    return
+  fi
+
+  local orig_dir=$(pwd)
+  pushd $INSTALL_DIR > /dev/null 2>&1  # {
+
+    #
+    # Remove, no questions asked, the old data base, and make a new empty one
+    #
+    ${MYSQL} --user=root --password=root -e "drop   database if exists $db_name;"
+    ${MYSQL} --user=root --password=root -e "create database           $db_name;"
+
+    if true ; then
+      #
+      # Remove, no questions asked, the old source tree, prior to git cloning
+      #
+      rm -rf $workdir
+      git clone https://github.com/andrew13/Laravel-4-Bootstrap-Starter-Site $workdir
+    fi
+    pushd $workdir > /dev/null 2>&1  # {
+      git checkout $version
+      local stable_composer=${orig_dir}/../scripts/composer_installer.phar
+      if [ ! -e "${stable_composer}" ] ; then
+        curl --silent http://getcomposer.org/installer > ${stable_composer}
+      fi
+
+      #
+      # TODO: Jun2014: This works with the zend php, but not with the hhvm php
+      #
+      if [ -e "${stable_composer}" ] ; then
+        cat ${stable_composer} | ${PHP}
+      else
+        curl --silent http://getcomposer.org/installer | ${PHP}
+      fi
+
+      #
+      # Patch the composer.json file as necessary
+      #
+      if [ -n "${composer_patch_function}" ] ; then
+        $composer_patch_function composer.json
+      fi
+
+      #
+      # This step can be quite slow,
+      # especially if the New Relic agent is logging extensively
+      # composer kind of sucks in this regard.
+      #
+      ${PHP} composer.phar install --dev
+    popd > /dev/null 2>&1  # }
+
+    #
+    # Fill in configuration files
+    #
+    local hostname=$(hostname)
+    pushd $workdir > /dev/null 2>&1  # {
+       < bootstrap/start.php \
+        sed \
+          -e "s/'local' =>.*/'local' => array('${hostname}'),/" \
+          -e "s/'staging' =>.*/'staging' => array('${hostname}'),/" \
+          -e "s/'production' =>.*/'production' => array('${hostname}'),/" \
+        > bootstrap/new.start.php
+      mv bootstrap/new.start.php bootstrap/start.php
+
+      mkdir -p app/config/local || true
+
+        # -e "s#'url'.*#'url' => '$BASE_URL/$workdir',#"
+      local xBASE_URL=http://127.0.0.1:${port}
+
+      < app/config/app.php \
+      sed \
+        -e "s#'url'.*#'url' => '$xBASE_URL',#" \
+        -e "s#'key'.*#'key' => 'KEY',#" \
+      > app/config/new.app.php
+
+      cp app/config/new.app.php app/config/app.php
+      cp app/config/new.app.php app/config/local/app.php
+
+      < app/config/database.php \
+      sed \
+        -e "s/'database'.*=>.*/'database' => '$db_name',/" \
+        -e "s/'username'.*=>.*/'username' => 'root',/" \
+        -e "s/'password'.*=>.*/'password' => 'root',/" \
+        -e "s#'key'.*#'key' => 'KEY',#" \
+      > app/config/new.database.php
+      cp app/config/new.database.php app/config/local/database.php
+      cp app/config/new.database.php app/config/database.php
+
+      ${PHP} artisan migrate
+      ${PHP} artisan db:seed
+
+      #
+      # It is not clear if we really need this key
+      #
+      local key=$(${PHP} artisan key:generate --env=local)
+
+      #
+      # get Laravel version information in two different ways
+      #
+      grep VERSION ./vendor/laravel/framework/src/Illuminate/Foundation/Application.php
+      ${PHP} artisan --version
+
+      if wget --quiet --output-document=- $BASE_URL/$workdir/public/index.php | grep --silent -i 'lorem' ; then
+        echo "Web Server returns LOREM IPSUM.  This is good!"
+      else
+        echo "Web Server is not returning desired content.  This is bad."
+      fi
+
+    popd > /dev/null 2>&1 # }
+
+  popd > /dev/null 2>&1 # }
+}
+
+laravel40_composer_patch_function() {
+  #
+  # TODO: sed in place composer.json
+  #
+  # This seems to be a consistent set of composer dependencies,
+  # snapshotted around of 2013-10-21
+  # to support Laravel 4.0.9
+  # This was determined empirically by looking at packagist:
+  #   https://packagist.org/packages/zizaco/confide
+  #   https://packagist.org/packages/zizaco/entrust
+  #   https://packagist.org/packages/laravel/framework
+  #
+  < composer.json \
+  sed \
+    -e 's#"laravel/framework".*#"laravel/framework": "4.0.9",#' \
+    -e 's#"zizaco/confide".*#"zizaco/confide": "2.0.0b4",#' \
+    -e 's#"zizaco/entrust".*#"zizaco/entrust": "v0.4.0beta",#' \
+  > composer.json.new
+  mv composer.json.new composer.json
+}
+
+null_composer_patch_function() {
+  : empty
+}
+
+if [ "${do_laravel40}" == "yes" ] ; then
+  workdir=laravel40
+  db_name=laravel40
+  version=bfa33379608549bf929862cd055b265168ea7570
+  do_work laravel40 laravel40 $version laravel40_composer_patch_function
+fi
+
+if [ "${do_laravel41}" == "yes" ] ; then
+  workdir=laravel41
+  db_name=laravel41
+  version=master
+  do_work laravel41 laravel41 $version null_composer_patch_function
+fi
---
+++ installmagento.sh
@@ -0,0 +1,258 @@
+#! /bin/bash
+
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 New Relic
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
+
+#
+# Use this script to install magento CE 1.6, 1.7 or 1.8.
+#
+# The script fills a mysql database named "magento16", "magento17" or "magento18"
+# with sample data for the sample store front
+#
+# This script can install the magento2 distribution from github.
+# As of 11mar2014, there are no data base dumps for a sample magento2 store front.
+#
+# Edit the admin names, below, as necessary.
+# Note that the admin_password has to be pretty strong for Magento to accept it.
+#
+# There must be localhost httpd server running
+#
+
+set -e
+set -u
+
+do_stock_lamp=yes
+
+do_force=no    # overwrite an existing installation
+do_install=no  # Run the magento installation script
+do_yireo=no
+
+do_magento16=no
+do_magento17=no
+do_magento18=no
+do_magento20=no  # not fully implemented
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --nrlamp|--do_nrlamp)
+      do_stock_lamp=no
+      ;;
+    --force|--do_force)
+      do_force=yes
+      ;;
+    --install|--do_install)
+      do_install=yes
+      ;;
+    --yireo|--do_yireo)
+      do_yireo=yes
+      ;;
+    --magento16|--do_magento16)
+      do_magento16=yes
+      ;;
+    --magento17|--do_magento17)
+      do_magento17=yes
+      ;;
+    --magento18|--do_magento18)
+      do_magento18=yes
+      ;;
+    --magento20|--do_magento20)
+      do_magento20=yes
+      ;;
+  esac
+  shift
+done
+
+#
+# Perhaps follow instructions here:
+#   http://www.magentocommerce.com/wiki/1_-_installation_and_configuration/installing_magento_via_shell_ssh#installing_magento_with_the_full_download_sample_data
+#
+
+if [ "${do_stock_lamp}" == "yes" ] ; then
+  INSTALL_DIR=/var/www/frameworks
+  URL_PATH="localhost:80/frameworks/"
+else
+  INSTALL_DIR=/opt/nr/htdocs/frameworks
+  URL_PATH="localhost:9090/frameworks/"
+fi
+if [ ! -e ${INSTALL_DIR} ] ; then
+  mkdir -p ${INSTALL_DIR}
+fi
+
+#
+# Usage: do_install_magento $workdir $db_name $version $install_file
+#
+do_install_magento() {
+  workdir=$1
+  db_name=$2
+  version=$3
+  install_file=$4
+
+  #
+  # if the work directory already exists, presume that we've been successful before
+  #
+  if [ "${do_force}" == "no" -a -e "${workdir}" ] ; then
+    echo "Re-using pre-existing magento installation ${workdir} ${version}"
+    return
+  fi
+
+  #
+  # Download, as needed, the tar files
+  #
+  if [ ! -e magento-${version}.tar.gz ] ; then
+    echo "wget magento source"
+    wget http://www.magentocommerce.com/downloads/assets/${version}/magento-${version}.tar.gz
+  fi
+
+  if [ ! -e magento-sample-data-1.6.1.0.tar.gz ] ; then
+    echo "wget magento demo store data"
+    wget http://www.magentocommerce.com/downloads/assets/1.6.1.0/magento-sample-data-1.6.1.0.tar.gz
+  fi
+
+  if [ ! -e Yireo_NewRelic-1.2.0.tgz ] ; then
+    echo "wget magento yireo 1.2.0 extension"
+    wget http://freegento.com/magento-extensions/Yireo_NewRelic-1.2.0.tgz
+  fi
+
+  if [ ! -e Yireo_NewRelic-1.2.1.tgz ] ; then
+    if false ; then
+      #
+      # 1.2.1 released on 11Mar2014, but it isn't installed as a sibling to 1.2.0
+      # Robert got it another way.  Diffing, the only difference between 1.2.0 and 1.2.1 is
+      # the change of license from OSL to BSD.
+      #
+      echo "wget magento yireo 1.2.1 extension"
+      wget http://freegento.com/magento-extensions/Yireo_NewRelic-1.2.1.tgz
+    fi
+  fi
+
+  echo "install magento-${version}"
+  rm -rf magento || true     # the tar files create a top level directory magento
+  tar xf magento-${version}.tar.gz
+  rm -rf ${workdir} || true  # Remove any previous instane of the working directory
+  mv magento ${workdir}
+
+  echo "install magento sample data"
+  rm -rf magento-sample-data-1.6.1.0 || true
+  tar xf magento-sample-data-1.6.1.0.tar.gz
+
+  mv magento-sample-data-1.6.1.0/media/*                             ${workdir}/media/
+  mv magento-sample-data-1.6.1.0/magento_sample_data_for_1.6.1.0.sql ${workdir}/data.sql
+
+  chmod -R o+w ${workdir}/media ${workdir}/var
+  chmod -R o+w ${workdir}/var   ${workdir}/var/.htaccess ${workdir}/app/etc
+
+  if [ "${do_yireo}" == "yes" ] ; then
+    pushd ${workdir}
+      tar zxvf ../Yireo_NewRelic-1.2.0.tgz
+      ls -l app/code/community
+    popd
+  fi
+
+  #
+  # That's right, we just unpacked the magento-sample-data into the magento directory,
+  # but we presume that is OK for magento2 as well (TODO: check this assumption)
+  #
+  # Per https://github.com/magento/magento2/issues/30 (2012 timeframe)
+  # Magento1.x data dumps are incompatible with Magento2.x data dumps
+  #
+  pushd ${workdir} > /dev/null 2>&1
+    echo "rebuild database using ${db_name}"
+    which mysql
+    mysql --user=root --password=root -e "drop database if exists ${db_name}"
+    mysql --user=root --password=root -e "create database         ${db_name}"
+    if [ "${do_magento20}" == "yes" ] ; then
+      echo "No sample data for magento2 installations"
+    else
+      mysql --user=root --password=root                           ${db_name} < data.sql
+    fi
+  popd
+
+  #
+  # Run the installation script.
+  # The apached/httpd must be running on the required port
+  #
+  # The fw_magento framework engine in the agent must be functioning.
+  #
+  if [ "${do_install}" == "yes" ] ; then
+    echo "run install script"
+
+    pushd ${workdir} > /dev/null 2>&1
+      if [ -e "${install_file}" ] ; then
+
+        #
+        # TODO: This doesn't work for magento2 (10Mar2014)
+        #
+        php -f "${install_file}" -- \
+          --license_agreement_accepted "yes" \
+          --locale "en_US" \
+          --timezone "America/Los_Angeles" \
+          --default_currency "USD" \
+          --db_host "localhost" \
+          --db_name "${db_name}" \
+          --db_user "root" \
+          --db_pass "root" \
+          --url "${URL_PATH}${workdir}" \
+          --use_rewrites "yes" \
+          --use_secure "no" \
+          --secure_base_url "" \
+          --use_secure_admin "no" \
+          --admin_firstname "Magento" \
+          --admin_lastname "Magento" \
+          --admin_username "magento" \
+          --admin_password "magento4now"
+
+      else
+        echo "no install.php script found"
+      fi
+
+    popd
+  fi
+}
+
+pushd ${INSTALL_DIR} > /dev/null 2>&1  # {
+
+if [ "${do_magento20}" == "yes" ] ; then
+  workdir=magento2
+  db_name=magento2
+  version="?"
+  install_file="dev/shell/install.php"
+  if [ ! -e "magento2" ] ; then
+    git clone git@github.com:magento/magento2.git
+  fi
+  do_install_magento magento2 magento2 '?' $install_file
+fi
+
+if [ "${do_magento16}" = "yes" ] ; then
+  do_install_magento magento16 magento16 1.6.2.0 install.php
+fi
+
+if [ "${do_magento17}" = "yes" ] ; then
+  do_install_magento magento17 magento17 1.7.0.0 install.php
+fi
+
+if [ "${do_magento18}" = "yes" ] ; then
+  do_install_magento magento18 magento18 1.8.1.0 install.php
+fi
+
+popd  # }
